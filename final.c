921c01a82d68f6d15f9e51ebcc517af6615431d3
สิงอ้วนนกหยีลาดกระบัง <sailom.01216@gmail.com>
2025-09-16 14:44:11 +0700

    Update final.c

--- final.c
+++ final.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void Open();
+void Read();
+void Search();
+void Add();
+void Update();
+void Delete();
+
 int main()
 {
-    
+    printf("-------- ระบบจัดการข้อมูลการสมัครสมาชิก --------\n");
+    printf("1.อ่านข้อมูล\n");
+    printf("2.เพิ่มข้อมูล\n");
+    printf("3.ค้นหาข้อมูล\n");
+    printf("4.อัพเดทข้อมูล\n");
+    printf("5.ลบข้อมูล");
+
 }
+
